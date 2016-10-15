bc9daf92584ca348e2cc0ea343a9643de8a36c08
SadchikovDmitry <dmi.sadchikov@gmail.com>
2016-10-15 19:27:09 +0300

    add tests

---
+++ TestSet/src/test/java/SimpleSetImplTest.java
@@ -0,0 +1,43 @@
+import org.junit.Before;
+import org.junit.Test;
+
+public class SimpleSetImplTest {
+
+    SimpleSet<String> stringSimpleSet;
+
+    @Before
+    public void setUp() throws Exception {
+        stringSimpleSet = new SimpleSetImpl<>();
+    }
+
+    @Test
+    public void add() throws Exception {
+
+        for (int i = 0; i < 15; i++) {
+            stringSimpleSet.add("s" + i);
+        }
+        System.out.println("Добавление элементов: ");
+        showSet();
+
+    }
+
+    @Test
+    public void find() throws Exception {
+        add();
+        System.out.println("Индекс найденного элемента: " + stringSimpleSet.find("s9"));
+    }
+
+    @Test
+    public void delete() throws Exception {
+        add();
+        stringSimpleSet.delete("s3");
+        System.out.println("После удаления: ");
+        showSet();
+    }
+
+    @Test
+    public void showSet() throws Exception {
+        stringSimpleSet.showSet();
+    }
+
+}
